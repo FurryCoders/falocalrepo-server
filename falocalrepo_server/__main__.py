2e8d2028325e7431befd6c1f0a3420718eb850c5
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-09-20 17:46:14 +0200

    main - assert FA.db is present before running app

--- falocalrepo_server/__main__.py
+++ falocalrepo_server/__main__.py
@@ -1,9 +1,11 @@
+from os.path import isfile
 from sys import argv
 
 from .server import app
 
 
 def main():
+    assert isfile("FA.db")
     host, port = argv[1].split(":") if argv[1:] else ("0.0.0.0", 8080)
     app.run(host=host, port=int(port))
 
