4836791ee3eec83df64f3fce2a721d34a63ca130
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-10-16 01:59:19 +0200

    __main__ - main uses server()

--- falocalrepo_server/__main__.py
+++ falocalrepo_server/__main__.py
@@ -1,14 +1,14 @@
 from os.path import isfile
 from sys import argv
 
-from .server import app
+from .server import server
 
 
 def main():
     assert isfile(argv[1])
 
     host, port = argv[2].split(":") if argv[2:] else ("0.0.0.0", 8080)
-    app.run(argv[1], host=host, port=int(port))
+    server(argv[1], host=host, port=int(port))
 
 
 if __name__ == '__main__':
