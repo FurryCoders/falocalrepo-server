e77be043f13fb14075a19472f1a451479eec6899
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-12-29 18:21:55 +0100

    __main__ - raise FileNotFoundError instead of basic assertion

--- falocalrepo_server/__main__.py
+++ falocalrepo_server/__main__.py
@@ -5,7 +5,8 @@ from .server import server
 
 
 def main():
-    assert isfile(argv[1])
+    if not isfile(argv[1]):
+        raise FileNotFoundError(argv[1])
 
     host, port = argv[2].split(":") if argv[2:] else ("0.0.0.0", 8080)
     server(argv[1], host=host, port=int(port))
