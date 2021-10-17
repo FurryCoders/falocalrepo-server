9a24edfabb0beb974e9ecfef354b68f7fc579a06
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-10-17 11:23:54 +0200

    __main__ - add help messages to arguments, raise error if missing SSL cert/key

--- falocalrepo_server/__main__.py
+++ falocalrepo_server/__main__.py
@@ -1,3 +1,5 @@
+from argparse import Action
+from argparse import ArgumentError
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -6,16 +8,25 @@ from .server import server
 
 def main():
     argparser: ArgumentParser = ArgumentParser(description="Web interface for https://pypi.org/project/falocalrepo/.")
-    argparser.add_argument("database", type=Path)
-    argparser.add_argument("--host", type=str, default="0.0.0.0")
-    argparser.add_argument("--port", type=int, default=None)
-    argparser.add_argument("--ssl-cert", dest="ssl_cert", type=Path, default=None)
-    argparser.add_argument("--ssl-key", dest="ssl_key", type=Path, default=None)
+    argparser.add_argument("database", type=Path,
+                           help="path to FALocalRepo database file")
+    argparser.add_argument("--host", type=str, default="0.0.0.0",
+                           help="server host, defaults to 0.0.0.0")
+    argparser.add_argument("--port", type=int, default=None,
+                           help="server port, defaults to 80 for HTTP and 443 for HTTPS")
+    ssl_cert_action: Action = argparser.add_argument("--ssl-cert", dest="ssl_cert", type=Path, default=None,
+                                                     help="path to SSL certificate file for HTTPS")
+    ssl_key_action: Action = argparser.add_argument("--ssl-key", dest="ssl_key", type=Path, default=None,
+                                                    help="path to SSL key file for HTTPS")
 
     args = argparser.parse_args()
 
     if not args.database.is_file():
         raise FileNotFoundError(args.database)
+    elif args.ssl_cert and not args.ssl_key:
+        raise ArgumentError(ssl_key_action, "SSL certificate must be accompanied by a key")
+    elif args.ssl_key and not args.ssl_cert:
+        raise ArgumentError(ssl_cert_action, "SSL key must be accompanied by a certificate")
 
     server(args.database, host=args.host, port=args.port, ssl_cert=args.ssl_cert, ssl_key=args.ssl_key)
 
